f71737a177b743bf15d2a121befe5d1f8ef1b800
Sooraj Singh <sooraj12002@gmail.com>
2015-06-30 16:28:43 +0530

    Commit after adding the gitignore file

---
+++ MVC5BootstrapDRAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5BootstrapDRAdmin.Startup))]
+namespace MVC5BootstrapDRAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
